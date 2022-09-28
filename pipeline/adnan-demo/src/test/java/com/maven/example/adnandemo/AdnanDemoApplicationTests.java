f2c6c2e69bd83103303c823b16fc1851b6ddde4b
adnan-ktm <114104550+adnan-ktm@users.noreply.github.com>
2022-09-28 14:04:18 -0400

    Add Maven Project

---
+++ pipeline/adnan-demo/src/test/java/com/maven/example/adnandemo/AdnanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.example.adnandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnanDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
