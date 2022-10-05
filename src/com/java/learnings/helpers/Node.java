6afd679a8f4d75a69dd1e6561aa716e290317e23
adnan-ktm <114104550+adnan-ktm@users.noreply.github.com>
2022-10-05 16:35:50 -0400

    Add linked list helper

---
+++ src/com/java/learnings/helpers/Node.java
@@ -0,0 +1,28 @@
+package com.java.learnings.helpers;
+
+public class Node {
+
+	public Node next;
+	public int data;
+
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+}
