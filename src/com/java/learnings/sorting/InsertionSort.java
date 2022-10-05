d0147cd5632418a8bc05033ae4e66c6540b7a286
adnan-ktm <114104550+adnan-ktm@users.noreply.github.com>
2022-10-05 16:29:31 -0400

    Iterative Insertion Sort

--- src/com/java/learnings/sorting/InsertionSort.java
+++ src/com/java/learnings/sorting/InsertionSort.java
@@ -2,4 +2,31 @@ package com.java.learnings.sorting;
 
 public class InsertionSort {
 
+	private static void insertionSortRecursive(int[] input, int i) {
+		if (i <= 1) {
+			return;
+		}
+		insertionSortRecursive(input, i - 1);
+		int key = input[i - 1];
+		int j = i - 2;
+		while (j >= 0 && input[j] > key) {
+			input[j + 1] = input[j];
+			j = j - 1;
+		}
+		input[j + 1] = key;
+	}
+
+	public static void insertionSortIterative(int[] input) {
+		for (int i = 1; i < input.length; i++) {
+			int key = input[i];
+			int j = i - 1;
+			while (j >= 0 && input[j] > key) {
+				input[j + 1] = input[j];
+				j = j - 1;
+			}
+			input[j + 1] = key;
+		}
+	}
+//Reference Baeldung
 }
+
